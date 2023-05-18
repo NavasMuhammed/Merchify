a5b199ea462b5da3292e89b1dd9f14f1e33e2c5a
NavasMuhammed <navas3444a3@gmail.com>
2023-05-18 13:50:25 +0530

    feat: hero section

--- src/components/hero/heroSection.tsx
+++ src/components/hero/heroSection.tsx
@@ -1,8 +1,22 @@
+import "./hero.scss";
 const HeroSection = () => {
     return (
-        <>
-            <h1>hero section</h1>
-        </>
+        <div className="hero-container">
+            <div className="hero-title">
+                <div>
+                    <span id="violet">The</span>
+                    <span id="green">Daily</span>
+                    <span id="violet">MS</span>
+                </div>
+                <div>
+                    <span id="green">Excel</span>
+                    <span id="violet">Quiz</span>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="/assets/images/hero image.png" alt="" />
+            </div>
+        </div>
     );
 };
 
