bd68f08848b3aefb173b4361e59e530645bd3285
NavasMuhammed <navas3444a3@gmail.com>
2023-05-19 14:02:23 +0530

    feat: score-card

--- src/pages/result/result.tsx
+++ src/pages/result/result.tsx
@@ -7,10 +7,25 @@ const Result = () => {
     return (
         <>
             <TopBar />
-            <div className='footer-container'>
-                <button  >
-                    Answers
-                </button>
+            <div className='result-container'>
+                <span className='result-title'>The Daily MS Excel Quiz</span>
+                <div className="score-card">
+                    <span>You Scored:</span>
+                    <div className="percentage-wrapper">
+                        <img src="/assets/images/pattern.png" alt="" />
+                        <span>80%</span>
+                        <img src="/assets/images/pattern.png" alt="" />
+                    </div>
+                    <span>Oh Snap, You can do better!</span>
+                    <p>
+                        Success is not final,failure is no fatal;It is the courage to continue that counts.
+                    </p>
+                </div>
+                <div className='footer-container'>
+                    <button  >
+                        Answers
+                    </button>
+                </div>
             </div>
         </>
     );
